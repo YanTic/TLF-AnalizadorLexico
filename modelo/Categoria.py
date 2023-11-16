2e3f8887d6dbc4bcd3d9e3d0258750dc46aa8354
Juan Felipe Lopez <juanfelipelopez55@gmail.com>
2023-11-15 22:04:29 -0400

    Categoria con palabras reservadas seleccionadas hasta el momento admitidas

--- modelo/Categoria.py
+++ modelo/Categoria.py
@@ -8,3 +8,73 @@ class Categoria:
 
         return True
 
+    def esPalabraReservada(cadena):
+        opc = 0
+        caracterActual = cadena[0]
+        if caracterActual == 'S':
+            opc = 0
+        elif caracterActual == 'F':
+            opc = 1
+        elif caracterActual == 'E':
+            opc = 2
+        elif caracterActual == 'V':
+            opc = 3
+        elif caracterActual == 'C':
+            opc = 4
+        elif caracterActual == 'B':
+            opc = 5
+        else:
+            return False
+
+        for i in range(1, len(cadena)):
+            caracterActual = cadena[i]
+            if opc == 0:
+                if caracterActual == 'I':
+                    return True
+                else:
+                    return False
+            elif opc == 1:
+                if i == 1 and caracterActual == 'U':
+                    continue
+                elif i == 2 and caracterActual == 'N':
+                    continue
+                elif i == 3 and caracterActual == 'C':
+                    return True
+                else:
+                    return False
+            elif opc == 2:
+                if i == 1 and caracterActual == 'N':
+                    continue
+                elif i == 2 and caracterActual == 'T':
+                    return True
+                else:
+                    return False
+            elif opc == 3:
+                if i == 1 and caracterActual == 'A':
+                    continue
+                elif i == 2 and caracterActual == 'R':
+                    return True
+                else:
+                    return False
+            elif opc == 4:
+                if i == 1 and caracterActual == 'A':
+                    continue
+                elif i == 2 and caracterActual == 'D':
+                    return True
+                else:
+                    return False
+            elif opc == 5:
+                if i == 1 and caracterActual == 'O':
+                    continue
+                elif i == 2 and caracterActual == 'O':
+                    continue
+                elif i == 3 and caracterActual == 'L':
+                    return True
+                else:
+                    return False
+
+        return False
+
+
+
+
