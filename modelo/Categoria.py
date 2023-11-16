e30470d81e0d2a610369e21aaee81510e58d8d6f
Juan Felipe Lopez <juanfelipelopez55@gmail.com>
2023-11-15 21:15:28 -0400

    categoria con solo la funcion de numeros enteros pero pa que todos trabajemos sobre el mismo archivo

---
+++ modelo/Categoria.py
@@ -0,0 +1,10 @@
+class Categoria:
+    def esNumeroNatural(cadena):
+        numerosNaturales = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+        for caracterActual in cadena:
+            if caracterActual not in numerosNaturales:
+                return False
+
+        return True
+
