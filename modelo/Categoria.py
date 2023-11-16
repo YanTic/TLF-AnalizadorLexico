99bd62c1000c493e4e55c53f1b2cfd37cac2b8c7
Juan Felipe Lopez <juanfelipelopez55@gmail.com>
2023-11-16 12:07:51 -0400

    dos de operadores y corrección de errores anteriores

--- modelo/Categoria.py
+++ modelo/Categoria.py
@@ -1,11 +1,19 @@
 class Categoria:
-    def esNumeroNatural(cadena):
-        numerosNaturales = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-
-        for caracterActual in cadena:
-            if caracterActual not in numerosNaturales:
+    def esNumeroEntero(cadena):
+        numerosNaturales = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        inicio = 0
+        negativo = cadena[0]
+        if negativo == "-":
+            inicio = 1
+        for i in range(inicio, len(cadena)):
+            caracterActual = cadena[i]
+            esDigitoNatural = False
+            for digito in numerosNaturales:
+                if caracterActual == digito:
+                    esDigitoNatural = True
+                    break
+            if not esDigitoNatural:
                 return False
-
         return True
 
     def esPalabraReservada(cadena):
@@ -29,7 +37,7 @@ class Categoria:
         for i in range(1, len(cadena)):
             caracterActual = cadena[i]
             if opc == 0:
-                if caracterActual == 'I':
+                if caracterActual == 'I' and i == len(cadena) -1:
                     return True
                 else:
                     return False
@@ -38,28 +46,28 @@ class Categoria:
                     continue
                 elif i == 2 and caracterActual == 'N':
                     continue
-                elif i == 3 and caracterActual == 'C':
+                elif i == 3 and caracterActual == 'C' and i == len(cadena) -1:
                     return True
                 else:
                     return False
             elif opc == 2:
                 if i == 1 and caracterActual == 'N':
                     continue
-                elif i == 2 and caracterActual == 'T':
+                elif i == 2 and caracterActual == 'T' and i == len(cadena) -1:
                     return True
                 else:
                     return False
             elif opc == 3:
                 if i == 1 and caracterActual == 'A':
                     continue
-                elif i == 2 and caracterActual == 'R':
+                elif i == 2 and caracterActual == 'R' and i == len(cadena) -1:
                     return True
                 else:
                     return False
             elif opc == 4:
                 if i == 1 and caracterActual == 'A':
                     continue
-                elif i == 2 and caracterActual == 'D':
+                elif i == 2 and caracterActual == 'D' and i == len(cadena) -1:
                     return True
                 else:
                     return False
@@ -68,11 +76,62 @@ class Categoria:
                     continue
                 elif i == 2 and caracterActual == 'O':
                     continue
-                elif i == 3 and caracterActual == 'L':
+                elif i == 3 and caracterActual == 'L' and i == len(cadena) -1:
                     return True
                 else:
                     return False
 
+        return False
+
+    def esOperadorLogico(cadena):
+        opc = 0
+        caracterActual = cadena[0]
+        if caracterActual == 'A':
+            opc = 0
+        elif caracterActual == 'O':
+            opc = 1
+        elif caracterActual == 'N':
+            opc = 2
+        else:
+            return False
+
+        for i in range(1, len(cadena)):
+            caracterActual = cadena[i]
+            if opc == 0:
+                if i == 1 and caracterActual == 'N':
+                    continue
+                if i == 2 and caracterActual == 'D' and i == len(cadena) -1:
+                    return True
+                else:
+                    return False
+            if opc == 1:
+                if i == 1 and caracterActual == 'R' and i == len(cadena) -1:
+                    return True
+                else:
+                    return False
+            if opc == 2:
+                if i == 1 and caracterActual == 'O':
+                    continue
+                if i == 2 and caracterActual == 'T' and i == len(cadena) -1:
+                    return True
+                else:
+                    return False
+
+        return False
+
+    def esOperadorAritmetico(cadena):
+        caracterActual = cadena[0]
+        if len(cadena) == 1:
+            if caracterActual == '+':
+                return True
+            elif caracterActual == '-':
+                return True
+            elif caracterActual == '*':
+                return True
+            elif caracterActual == '/':
+                return True
+            elif caracterActual == '%':
+                return True
         return False
 
 
